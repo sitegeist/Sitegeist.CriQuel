70f6ed1dbab9707f3066bd4aef0197f69ceb1b41
Martin Ficzel <ficzel@sitegeist.de>
2023-09-21 19:00:27 +0200

    TASK: Raise phpstan level to 9 and adjust matchesPropertyConstraint method

--- Classes/Matcher/NodeCriteriaMatcher.php
+++ Classes/Matcher/NodeCriteriaMatcher.php
@@ -44,19 +44,34 @@ final class NodeCriteriaMatcher
 
     public static function matchesPropertyConstraint(Node $node, PropertyValueCriteriaInterface $propertyValueCriteria): bool
     {
-        return match (get_class($propertyValueCriteria)) {
-            AndCriteria::class => self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria1) && self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria2),
-            OrCriteria::class=> self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria1) || self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria2),
-            NegateCriteria::class => !self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria),
-            PropertyValueContains::class => str_contains($node->getProperty($propertyValueCriteria->propertyName->value), $propertyValueCriteria->value),
-            PropertyValueEndsWith::class => str_ends_with($node->getProperty($propertyValueCriteria->propertyName->value), $propertyValueCriteria->value),
-            PropertyValueEquals::class => $node->getProperty($propertyValueCriteria->propertyName->value) == $propertyValueCriteria->value,
-            PropertyValueGreaterThan::class => $node->getProperty($propertyValueCriteria->propertyName->value) > $propertyValueCriteria->value,
-            PropertyValueGreaterThanOrEqual::class => $node->getProperty($propertyValueCriteria->propertyName->value) >= $propertyValueCriteria->value,
-            PropertyValueLessThan::class => $node->getProperty($propertyValueCriteria->propertyName->value) < $propertyValueCriteria->value,
-            PropertyValueLessThanOrEqual::class => $node->getProperty($propertyValueCriteria->propertyName->value) <= $propertyValueCriteria->value,
-            PropertyValueStartsWith::class => str_starts_with($node->getProperty($propertyValueCriteria->propertyName->value), $propertyValueCriteria->value),
-            default => (throw new \InvalidArgumentException('unknown criteria of type ' . get_class($propertyValueCriteria)))
-        };
+        switch (true) {
+            case $propertyValueCriteria instanceof AndCriteria:
+                return self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria1) && self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria2);
+            case $propertyValueCriteria instanceof OrCriteria:
+                return self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria1) || self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria2);
+            case $propertyValueCriteria instanceof NegateCriteria:
+                return !self::matchesPropertyConstraint($node, $propertyValueCriteria->criteria);
+            case $propertyValueCriteria instanceof PropertyValueContains:
+                $propertyValue = $node->getProperty($propertyValueCriteria->propertyName->value);
+                return (is_string($propertyValue) || $propertyValue instanceof \Stringable) ? str_contains((string)$propertyValue, $propertyValueCriteria->value) : false;
+            case $propertyValueCriteria instanceof PropertyValueEndsWith:
+                $propertyValue = $node->getProperty($propertyValueCriteria->propertyName->value);
+                return (is_string($propertyValue) || $propertyValue instanceof \Stringable) ? str_ends_with((string)$propertyValue, $propertyValueCriteria->value) : false;
+            case $propertyValueCriteria instanceof PropertyValueStartsWith:
+                $propertyValue = $node->getProperty($propertyValueCriteria->propertyName->value);
+                return (is_string($propertyValue) || $propertyValue instanceof \Stringable) ? str_starts_with((string)$propertyValue, $propertyValueCriteria->value) : false;
+            case $propertyValueCriteria instanceof PropertyValueEquals:
+                return $node->getProperty($propertyValueCriteria->propertyName->value) == $propertyValueCriteria->value;
+            case $propertyValueCriteria instanceof PropertyValueGreaterThan:
+                return $node->getProperty($propertyValueCriteria->propertyName->value) > $propertyValueCriteria->value;
+            case $propertyValueCriteria instanceof PropertyValueGreaterThanOrEqual:
+                return $node->getProperty($propertyValueCriteria->propertyName->value) >= $propertyValueCriteria->value;
+            case $propertyValueCriteria instanceof PropertyValueLessThan:
+                return $node->getProperty($propertyValueCriteria->propertyName->value) < $propertyValueCriteria->value;
+            case $propertyValueCriteria instanceof PropertyValueLessThanOrEqual:
+                return $node->getProperty($propertyValueCriteria->propertyName->value) <= $propertyValueCriteria->value;
+            default:
+                throw new \InvalidArgumentException('unknown criteria of type ' . get_debug_type($propertyValueCriteria));
+        }
     }
 }
